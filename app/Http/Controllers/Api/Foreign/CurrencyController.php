c0a6088cfd3cb13eb787a48d1843d2cc383673e5
Mateusz Jasiewicz <m.jasiewicz@rolladengroup.de>
2022-06-16 21:42:49 +0200

    Utworzenie modelu, kontrolera i migracji dla currency

---
+++ app/Http/Controllers/Api/Foreign/CurrencyController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api\Foreign;
+
+use App\Http\Controllers\Controller;
+use App\Repository\CurrencyRepositoryInterface;
+use Illuminate\Http\Request;
+
+class CurrencyController extends Controller
+{
+    public function __construct(
+        private CurrencyRepositoryInterface $repository
+        )
+    {
+        $this->currencyRepositoryInterface = $repository;
+    }
+
+    public function index(Request $request)
+    {
+        $request->flash();
+
+        if ($request->query('phrase')) {
+            $validatedData = $request->validate([
+                'phrase' => 'string|nullable'
+            ]);
+        }
+        $phrase = $validatedData['phrase'] ?? null;
+
+        $allCurrency = $this->currencyRepositoryInterface->getAllFiltered($phrase);
+        return view('currency.index', [
+            'allCurrency' => $allCurrency,
+        ]);
+    }
+
+    public function downloadData()
+    {
+        $this->currencyRepositoryInterface->downloadData();
+        return redirect()
+            ->route('currency.index')
+            ->with('success', 'Pobrano najnowsze kursy walut');
+    }
+}
---
+++ database/migrations/2022_06_16_191108_create_currency_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currency', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 70);
+            $table->string('currency_code', 3)->unique('currency_code');
+            $table->float('exchange_rate', 6, 4, true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currency');
+    }
+};
