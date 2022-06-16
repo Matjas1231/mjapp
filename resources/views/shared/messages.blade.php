e744768ec1241b036539d8f6ccd5c4c3911ea687
Mateusz Jasiewicz <m.jasiewicz@rolladengroup.de>
2022-06-16 21:44:41 +0200

    Dodanie shared messages do widoków

---
+++ resources/views/shared/messages.blade.php
@@ -0,0 +1,13 @@
+@if ($message = Session::get('success'))
+    <div class="alert alert-success" role="alert">
+        {{-- <button type="button" class="btn-close" aria-label="Close">x</button> --}}
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
+
+@if ($message = Session::get('warning'))
+    <div class="alert alert-warning" role="alert">
+        {{-- <button type="button" class="btn-close" aria-label="Close">x</button> --}}
+        <strong>{{ $message }}</strong>
+    </div>
+@endif
