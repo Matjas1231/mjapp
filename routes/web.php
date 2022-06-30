c6333d926d1b25d614cdf09285e4869310cd3e93
Mateusz Jasiewicz <m.jasiewicz@rolladengroup.de>
2022-06-30 19:20:44 +0200

    Uruchomienie autoryzacji na routach

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\Foreign\CurrencyController;
 use App\Http\Controllers\Api\Foreign\ViesController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\ComputerController;
 use App\Http\Controllers\ComputerTypesController;
 use App\Http\Controllers\DashboardController;
@@ -12,7 +14,6 @@ use App\Http\Controllers\SoftwareController;
 use App\Http\Controllers\WorkerController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Nette\Schema\Context;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,139 +28,147 @@ use Nette\Schema\Context;
 
 Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
 
-// WORKERS
 Route::group([
-    'prefix' => 'workers',
-    'as' => 'worker.'
+    'middleware' => ['auth']
 ], function () {
-    Route::get('/workers/ajax-request-get', [WorkerController::class, 'ajaxList']);
+    // WORKERS
+    Route::group([
+        'prefix' => 'workers',
+        'as' => 'worker.'
+    ], function () {
+        Route::get('/workers/ajax-request-get', [WorkerController::class, 'ajaxList']);
 
-    Route::get('/', [WorkerController::class, 'list'])->name('list');
-    Route::get('/{workerId}/show', [WorkerController::class, 'show'])->name('show');
+        Route::get('/', [WorkerController::class, 'list'])->name('list');
+        Route::get('/{workerId}/show', [WorkerController::class, 'show'])->name('show');
 
-    Route::get('/create', [WorkerController::class, 'create'])->name('create');
-    Route::post('/store', [WorkerController::class, 'store'])->name('store');
+        Route::get('/create', [WorkerController::class, 'create'])->name('create');
+        Route::post('/store', [WorkerController::class, 'store'])->name('store');
 
-    Route::get('/{workerId}/edit', [WorkerController::class, 'edit'])->name('edit');
-    Route::post('/update', [WorkerController::class, 'update'])->name('update');
+        Route::get('/{workerId}/edit', [WorkerController::class, 'edit'])->name('edit');
+        Route::post('/update', [WorkerController::class, 'update'])->name('update');
 
-    Route::get('/{workerId}/delete', [WorkerController::class, 'delete'])->name('delete');
+        Route::get('/{workerId}/delete', [WorkerController::class, 'delete'])->name('delete');
+    });
+
+    // DEPARTMENTS
+    Route::group([
+        'prefix' => 'departments',
+        'as' => 'department.'
+    ], function () {
+        Route::get('/', [DepartmentController::class, 'list'])->name('list');
+
+        Route::get('/create', [DepartmentController::class, 'create'])->name('create');
+        Route::post('/store', [DepartmentController::class, 'store'])->name('store');
+
+        Route::get('/{departmentId}/edit', [DepartmentController::class, 'edit'])->name('edit');
+        Route::post('/update', [DepartmentController::class, 'update'])->name('update');
+
+        Route::get('/{departmentId}/delete', [DepartmentController::class, 'delete'])->name('delete');
+    });
+
+    // COMPUTERS TYPES
+    Route::group([
+        'prefix' => 'computers/types',
+        'as' => 'computer.type.'
+    ], function () {
+        Route::get('/', [ComputerTypesController::class, 'list'])->name('list');
+
+        Route::get('/{computerTypeId}/edit', [ComputerTypesController::class, 'edit'])->name('edit');
+        Route::post('/update', [ComputerTypesController::class, 'update'])->name('update');
+
+        Route::get('/create', [ComputerTypesController::class, 'create'])->name('create');
+        Route::post('/store', [ComputerTypesController::class, 'store'])->name('store');
+
+        Route::get('/{computerTypeId}/delete', [ComputerTypesController::class, 'delete'])->name('delete');
+    });
+
+    // COMPUTERS
+    Route::group([
+        'prefix' => 'computers',
+        'as' => 'computer.'
+    ], function () {
+        Route::get('/', [ComputerController::class, 'list'])->name('list');
+        Route::get('/{computerId}/show', [ComputerController::class, 'show'])->name('show');
+
+        Route::get('/create', [ComputerController::class, 'create'])->name('create');
+        Route::post('/store', [ComputerController::class, 'store'])->name('store');
+
+        Route::get('/{computerId}/edit', [ComputerController::class, 'edit'])->name('edit');
+        Route::post('/update', [ComputerController::class, 'update'])->name('update');
+
+        Route::get('/{computerId}/delete', [ComputerController::class, 'delete'])->name('delete');
+    });
+
+    // SOFTWARES
+    Route::group([
+        'prefix' => 'softwares',
+        'as' => 'software.'
+    ], function () {
+        Route::get('/', [SoftwareController::class, 'list'])->name('list');
+        Route::get('/{softwareId}/show', [SoftwareController::class, 'show'])->name('show');
+
+        Route::get('/create', [SoftwareController::class, 'create'])->name('create');
+        Route::post('/store', [SoftwareController::class, 'store'])->name('store');
+
+        Route::get('/{softwareId}/edit', [SoftwareController::class, 'edit'])->name('edit');
+        Route::post('/update', [SoftwareController::class, 'update'])->name('update');
+
+        Route::get('/{softwareId}/delete', [SoftwareController::class, 'delete'])->name('delete');
+    });
+
+    // PERIPHERALS
+    Route::group([
+        'prefix' => 'peripherals',
+        'as' => 'peripheral.'
+    ], function () {
+        Route::get('/', [PeripheralController::class, 'list'])->name('list');
+        Route::get('/{peripheralId}/show', [PeripheralController::class, 'show'])->name('show');
+
+        Route::get('/create', [PeripheralController::class, 'create'])->name('create');
+        Route::post('/store', [PeripheralController::class, 'store'])->name('store');
+
+        Route::get('/{peripheralId}/edit', [PeripheralController::class, 'edit'])->name('edit');
+        Route::post('/update', [PeripheralController::class, 'update'])->name('update');
+
+        Route::get('/{peripheralId}/delete', [PeripheralController::class, 'delete'])->name('delete');
+    });
+
+    // PERIPHERALS TYPES
+    Route::group([
+        'prefix' => 'peripherals/types',
+        'as' => 'peripheral.type.'
+    ], function () {
+        Route::get('/', [PeripheralTypeController::class, 'list'])->name('list');
+
+        Route::get('/{peripheralTypeId}/edit', [PeripheralTypeController::class, 'edit'])->name('edit');
+        Route::get('/update', [PeripheralTypeController::class, 'update'])->name('update');
+
+        Route::get('/create', [PeripheralTypeController::class, 'create'])->name('create');
+        Route::post('/store', [PeripheralTypeController::class, 'store'])->name('store');
+
+        Route::get('/{peripheralTypeId}/delete', [PeripheralTypeController::class, 'delete'])->name('delete');
+    });
+
+    Route::group([
+        'prefix' => 'currency',
+        'as' => 'currency.',
+    ], function () {
+        Route::get('/', [CurrencyController::class, 'index'])->name('index');
+        Route::get('/downloadcurrency', [CurrencyController::class, 'downloadData'])->name('downloadData');
+    });
+
+    Route::group([
+        'prefix' => 'vies',
+        'as' => 'vies.',
+    ], function () {
+        Route::get('/', [ViesController::class, 'index'])->name('index');
+    });
 });
 
-// DEPARTMENTS
-Route::group([
-    'prefix' => 'departments',
-    'as' => 'department.'
-], function () {
-    Route::get('/', [DepartmentController::class, 'list'])->name('list');
+// Authentication Routes
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
-    Route::get('/create', [DepartmentController::class, 'create'])->name('create');
-    Route::post('/store', [DepartmentController::class, 'store'])->name('store');
-
-    Route::get('/{departmentId}/edit', [DepartmentController::class, 'edit'])->name('edit');
-    Route::post('/update', [DepartmentController::class, 'update'])->name('update');
-
-    Route::get('/{departmentId}/delete', [DepartmentController::class, 'delete'])->name('delete');
-});
-
-// COMPUTERS TYPES
-Route::group([
-    'prefix' => 'computers/types',
-    'as' => 'computer.type.'
-], function () {
-    Route::get('/', [ComputerTypesController::class, 'list'])->name('list');
-
-    Route::get('/{computerTypeId}/edit', [ComputerTypesController::class, 'edit'])->name('edit');
-    Route::post('/update', [ComputerTypesController::class, 'update'])->name('update');
-
-    Route::get('/create', [ComputerTypesController::class, 'create'])->name('create');
-    Route::post('/store', [ComputerTypesController::class, 'store'])->name('store');
-
-    Route::get('/{computerTypeId}/delete', [ComputerTypesController::class, 'delete'])->name('delete');
-});
-
-// COMPUTERS
-Route::group([
-    'prefix' => 'computers',
-    'as' => 'computer.'
-], function () {
-    Route::get('/', [ComputerController::class, 'list'])->name('list');
-    Route::get('/{computerId}/show', [ComputerController::class, 'show'])->name('show');
-
-    Route::get('/create', [ComputerController::class, 'create'])->name('create');
-    Route::post('/store', [ComputerController::class, 'store'])->name('store');
-
-    Route::get('/{computerId}/edit', [ComputerController::class, 'edit'])->name('edit');
-    Route::post('/update', [ComputerController::class, 'update'])->name('update');
-
-    Route::get('/{computerId}/delete', [ComputerController::class, 'delete'])->name('delete');
-});
-
-// SOFTWARES
-Route::group([
-    'prefix' => 'softwares',
-    'as' => 'software.'
-], function () {
-    Route::get('/', [SoftwareController::class, 'list'])->name('list');
-    Route::get('/{softwareId}/show', [SoftwareController::class, 'show'])->name('show');
-
-    Route::get('/create', [SoftwareController::class, 'create'])->name('create');
-    Route::post('/store', [SoftwareController::class, 'store'])->name('store');
-
-    Route::get('/{softwareId}/edit', [SoftwareController::class, 'edit'])->name('edit');
-    Route::post('/update', [SoftwareController::class, 'update'])->name('update');
-
-    Route::get('/{softwareId}/delete', [SoftwareController::class, 'delete'])->name('delete');
-});
-
-// PERIPHERALS
-Route::group([
-    'prefix' => 'peripherals',
-    'as' => 'peripheral.'
-], function () {
-    Route::get('/', [PeripheralController::class, 'list'])->name('list');
-    Route::get('/{peripheralId}/show', [PeripheralController::class, 'show'])->name('show');
-
-    Route::get('/create', [PeripheralController::class, 'create'])->name('create');
-    Route::post('/store', [PeripheralController::class, 'store'])->name('store');
-
-    Route::get('/{peripheralId}/edit', [PeripheralController::class, 'edit'])->name('edit');
-    Route::post('/update', [PeripheralController::class, 'update'])->name('update');
-
-    Route::get('/{peripheralId}/delete', [PeripheralController::class, 'delete'])->name('delete');
-});
-
-// PERIPHERALS TYPES
-Route::group([
-    'prefix' => 'peripherals/types',
-    'as' => 'peripheral.type.'
-], function () {
-    Route::get('/', [PeripheralTypeController::class, 'list'])->name('list');
-
-    Route::get('/{peripheralTypeId}/edit', [PeripheralTypeController::class, 'edit'])->name('edit');
-    Route::get('/update', [PeripheralTypeController::class, 'update'])->name('update');
-
-    Route::get('/create', [PeripheralTypeController::class, 'create'])->name('create');
-    Route::post('/store', [PeripheralTypeController::class, 'store'])->name('store');
-
-    Route::get('/{peripheralTypeId}/delete', [PeripheralTypeController::class, 'delete'])->name('delete');
-});
-
-Route::group([
-    'prefix' => 'currency',
-    'as' => 'currency.',
-], function () {
-    Route::get('/', [CurrencyController::class, 'index'])->name('index');
-    Route::get('/downloadcurrency', [CurrencyController::class, 'downloadData'])->name('downloadData');
-});
-
-Route::group([
-    'prefix' => 'vies',
-    'as' => 'vies.',
-], function () {
-    Route::get('/', [ViesController::class, 'index'])->name('index');
-});
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
