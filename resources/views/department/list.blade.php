f929832d4bd5d78875a590b9bae39146ffae2eda
Mateusz Jasiewiczm <mat.jasiewicz@gmail.com>
2022-10-03 10:39:28 +0200

    Obsługa wyszukiwania działów

--- resources/views/department/list.blade.php
+++ resources/views/department/list.blade.php
@@ -8,10 +8,20 @@
 @endsection
 
 @section('content')
-    <table class="table table-striped">
+<div class="form-inline" id="filterForm">
+    <div class="form-group mb-2">
+        <label for="phrase">Wyszukaj: </label>
+    </div>
+    <div class="form-group mx-sm-3 mb-2">
+        <input type="text" class="form-control filter" id="filterdep" placeholder="Nazwa">
+    </div>
+</div>
+
+<div id="datatable">
+    <table class="table table-striped" id="datatable-table">
         <thead>
             <tr>
-                <th>Lp.</th>
+                <th>ID</th>
                 <th>Nazwa</th>
                 <th>Akcja</th>
             </tr>
@@ -19,7 +29,7 @@
         <tbody>
             @foreach ($departments as $department)
                 <tr>
-                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $department->id }}</td>
                     <td>{{ $department->name }}</td>
                     <td>
                         <a href="{{ route('department.edit', ['departmentId' => $department->id]) }}" class="btn btn-primary">Edytuj</a>
@@ -29,4 +39,12 @@
             @endforeach
         </tbody>
     </table>
+</div>
+<div id="resultdatatable"></div>
+@endsection
+
+@section('javascript')
+    <script>
+        searchDepartment("{{ route('department.searchDepartment') }}", "{{ csrf_token() }}");
+    </script>
 @endsection
