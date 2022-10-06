f60f028938c78313a0e398a4215a45d3a276d809
Mateusz Jasiewiczm <mat.jasiewicz@gmail.com>
2022-10-06 11:56:07 +0200

    JS i CSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
 require('./bootstrap');
 require('./sb-admin');
 require('./jquery-3.6.0.min');
+require('./prepareDataToSend');
 require('./deepl/translateDeepl');
 require('./departments/addDepartment');
 require('./departments/searchDepartment');
 require('./departments/deleteDepartment');
 require('./workers/searchWorker');
+require('./softwares/searchSoftware');
